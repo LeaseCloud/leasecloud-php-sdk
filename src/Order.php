918668df7bd76c570620298e40736e38724b60fb
Erik Torsner <erik@torgesta.com>
2017-08-25 15:27:30 +0200

    Adds status and cancel commands

--- src/Order.php
+++ src/Order.php
@@ -17,4 +17,35 @@ class Order extends ApiResource
     {
         return parent::create($order);
     }
+
+    /**
+     * Get status information about an order
+     *
+     * @param string $orderId
+     * @return mixed
+     */
+    public static function status($orderId)
+    {
+        $url = static::classUrl();
+        $url = $url . '/' . $orderId . '/status';
+        list($ret) = parent::staticRequest('get', $url, []);
+
+        return $ret;
+    }
+
+    /**
+     * Cancel an order
+     *
+     * @param string $orderId
+     * @return mixed
+     */
+    public static function cancel($orderId)
+    {
+        $url = static::classUrl();
+        $url = $url . '/' . $orderId . '/cancel';
+        list($ret) = parent::staticRequest('post', $url, []);
+
+        return $ret;
+    }
+
 }
