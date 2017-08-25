d47646f2c26ecd8d6085cb7cd2e39ee36747cb26
Erik Torsner <erik@torgesta.com>
2017-08-25 10:52:14 +0200

    PSR2 styles

--- src/Montly.php
+++ src/Montly.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace Montly;
-
 
 /**
  * Class Montly
@@ -10,29 +8,36 @@ namespace Montly;
  */
 class Montly
 {
-  // @var string The Montly API key to be used for requests.
-  public static $apiKey;
+    /**
+     * The Montly API key to be used for requests.
+     *
+     * @var string
+     */
+    public static $apiKey;
 
-  // @var string The base URL for the Montly API.
-  public static $apiBase = 'https://api.montly.com';
+    /**
+     * The base URL for the Montly API.
+     *
+     * @var string
+     */
+    public static $apiBase = 'https://api.montly.com';
 
-  /**
-   * Gets the API key to be used for requests.
-   *
-   * @param string $apiKey
-   */
-  public static function getApiKey()
-  {
-      return self::$apiKey;
-  }
+    /**
+     * Gets the API key to be used for requests.
+     *
+     */
+    public static function getApiKey()
+    {
+        return self::$apiKey;
+    }
 
-  /**
-   * Sets the API key to be used for requests.
-   *
-   * @param string $apiKey
-   */
-  public static function setApiKey($apiKey)
-  {
-      self::$apiKey = $apiKey;
-  }
+    /**
+     * Sets the API key to be used for requests.
+     *
+     * @param string $apiKey
+     */
+    public static function setApiKey($apiKey)
+    {
+        self::$apiKey = $apiKey;
+    }
 }
