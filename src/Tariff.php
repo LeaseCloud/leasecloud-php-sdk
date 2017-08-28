ca9e41af8a5499b581a22d48fa6874d307e4d9d6
Erik Torsner <erik@torgesta.com>
2017-08-28 14:48:47 +0200

    Adds public functions to calculate monthly price and determine correct tariff

--- src/Tariff.php
+++ src/Tariff.php
@@ -19,4 +19,47 @@ class Tariff extends ApiResource
     {
         return parent::retrieve($id, $params);
     }
+
+    /**
+     * Return the monthly cost for an item at $price leased for $months
+     * given the $tariffs in use for the Montly account
+     *
+     * @param double  $price   The price of the object
+     * @param integer $months  The agreement length
+     * @param object  $tariffs The tariffs object, as returned by Tariff:retrieve
+     *
+     * @return double|null The monthly cost for the end user
+     */
+    public static function monthlyCost($price, $months, $tariffs)
+    {
+
+        $tariff = static::tariff($months, $tariffs);
+        if (!is_null($tariff)) {
+            return $price * ($tariff / 100);
+        }
+
+        // If we're still here, it means that we don't have any tariff
+        // matching the number of months specified. Return null.
+        return null;
+    }
+
+    /**
+     * Return the $tariff for a contract at $months length
+     * given the tarrifs in use for the Montly account
+     *
+     * @param $months
+     * @param $tariffs
+     *
+     * @return double|null The tariff, or null if no tariff was found
+     */
+    public static function tariff($months, $tariffs)
+    {
+        foreach ($tariffs->tariffs as $tariff) {
+            if ($months == $tariff->months) {
+                return (double)($tariff->tariff);
+            }
+        }
+
+        return null;
+    }
 }
