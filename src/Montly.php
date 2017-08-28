c8712b080ba61c878d6a55ed2cab3dfbc3a3b218
Erik Torsner <erik@torgesta.com>
2017-08-28 14:47:34 +0200

    Refactoring. Webhook validation moved into separate class

--- src/Montly.php
+++ src/Montly.php
@@ -23,13 +23,6 @@ class Montly
     public static $apiBase = 'https://api.montly.com';
 
     /**
-     * The webhook secret for validating incomming webhook calls
-     *
-     * @var string
-     */
-    private static $webHookSecret = '';
-
-    /**
      * Gets the API key to be used for requests.
      *
      */
@@ -48,44 +41,5 @@ class Montly
         self::$apiKey = $apiKey;
     }
 
-    /**
-     * Sets the webhook secret
-     *
-     * @param string $secret
-     */
-    public static function setWebHookSecret($secret)
-    {
-        self::$webHookSecret = $secret;
-    }
 
-    /**
-     * Validate the webhook signature
-     *
-     * @param string $signature The string passed in header Montly-Signature
-     * @param string $payload   The raw payload
-     *
-     * @return bool True if the signature is valid, otherwise false
-     */
-    public static function validateSignature($signature, $payload)
-    {
-        $parts = explode(',', $signature);
-        $parameters = [];
-        foreach ($parts as $part) {
-            parse_str($part, $parsed);
-            $key = key($parsed);
-            $value = $parsed[$key];
-            if (!isset($parameters[$key])) {
-                $parameters[$key] = $value;
-            }
-        }
-
-        // We need timetamp and payload sig
-        if (!(isset($parameters['t']) && isset($parameters['v1']))) {
-            return false;
-        }
-
-        $wanted = hash_hmac('sha256', $parameters['t'] . $payload, self::$webHookSecret);
-
-        return $wanted === $parameters['v1'];
-    }
 }
