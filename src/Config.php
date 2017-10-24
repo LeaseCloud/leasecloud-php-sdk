53d5feb5783577cc57350ffe6dc3263aa15b6b12
Patrik Spathon <patrik@spathon.com>
2017-10-24 10:38:13 +0200

    Add custom classUrl on config

--- src/Config.php
+++ src/Config.php
@@ -8,6 +8,14 @@ namespace LeaseCloud;
 class Config extends ApiResource
 {
     /**
+     * @return string The endpoint URL for the given class.
+     */
+    public static function classUrl()
+    {
+        return "/v1/Config";
+    }
+
+    /**
      * Retreive (get) LeaseCloud config
      *
      * @param null $id
